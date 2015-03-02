38729e40b03e7b7f0873abfabe3040d014c90276
Jeff <jeff.saras@gmail.com>
2015-03-02 11:34:57 -0800

    P161 Finished abbr list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
+addLoadEvent(displayAbbreviations);
 
 function addLoadEvent(func) {
     var oldonload = window.onload;
@@ -28,34 +29,39 @@ function preparePlaceholder(){
     if(!document.getElementById) return false;
     if(!document.getElementById("imagegallery")) return false;
 
-    var placeholder = document.createElement("img");
+    var placeholder         = document.createElement("img");
     placeholder.setAttribute("id", "placeholder");
     placeholder.setAttribute("src", "images/DSC_0240.jpg");
     placeholder.setAttribute("alt", "my image gallery");
 
-    var description = document.createElement("p");
+    var description         = document.createElement("p");
     description.setAttribute("id", "description");
-    var desctext = document.createTextNode("Choose an image");
+    var desctext            = document.createTextNode("Choose an image");
 
     description.appendChild(desctext);
 
-    var gallery = document.getElementById("imagegallery");
+    var gallery             = document.getElementById("imagegallery");
     insertAfter(placeholder, gallery);
     insertAfter(description, placeholder);
 }
 
 function showPic(whichpic) {
     if (!document.getElementById("placeholder")) return true;
-    var source = whichpic.getAttribute("href");
-    var placeholder = document.getElementById("placeholder");
+
+    var source              = whichpic.getAttribute("href");
+    var placeholder         = document.getElementById("placeholder");
+
     if (placeholder.nodeName != "IMG") return true;
+
     placeholder.setAttribute("src", source);
     console.log(whichpic);
 
     if (!document.getElementById("description")) return false;
+
     var text = whichpic.getAttribute("title") ?
     whichpic.getAttribute("title") : "No Description";
-    var description = document.getElementById("description");
+
+    var description          = document.getElementById("description");
     if (description.firstChild.nodeType == 3) {
     	description.firstChild.nodeValue = text;
     }
@@ -69,13 +75,52 @@ function prepareGallery() {
     if (!document.getElementById("imagegallery")) return false;
 
     var gallery = document.getElementById("imagegallery");
-    var links = gallery.getElementsByTagName("a");
+    var links   = gallery.getElementsByTagName("a");
 
     for (var i = 0; i < links.length; i++) {
         links[i].onclick = function() {
             return showPic(this);
         }
     }
+}
+
+function displayAbbreviations() {
+    if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) return false;
+
+    var abbreviations = document.getElementsByTagName('abbr');
+
+    if (abbreviations.length < 1) return false;
+
+    var defs = new Array();
+
+    for (var i = 0; i < abbreviations.length; i++) {
+        var definition      = abbreviations[i].getAttribute('title');
+        var key             = abbreviations[i].lastChild.nodeValue;
+
+        defs[key]           = definition;
+    };
+
+    var dlist               = document.createElement('dl');
+
+    for (key in defs) {
+        var definition      = defs[key];
+        var dtitle          = document.createElement('dt');
+        var dtitle_text     = document.createTextNode(key);
+        dtitle.appendChild(dtitle_text);
+
+        var ddesc           = document.createElement('dd')  ;
+        var ddesc_text      = document.createTextNode(definition);
+
+        ddesc.appendChild(ddesc_text);
+        dlist.appendChild(dtitle);
+        dlist.appendChild(ddesc);
+    }
+
+    var header              = document.createElement('h2');
+    var header_text         = document.createTextNode("Abbreviations");
+    header.appendChild(header_text);
+    document.body.appendChild(header);
+    document.body.appendChild(dlist);
 }
 
 // window.onload = countBodyChildren;
